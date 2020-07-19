731b36f6d98d69e8c044fd8d4d19045fa7c1c522
Sabata <samofoke@student.wethinkcode.co.za>
2020-07-19 10:02:59 +0200

    basics

---
+++ lesson1/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int     main(void)
+{
+    int a;
+    int b;
+
+    a = 10;
+    b = 20;
+
+    a = a + 5;
+    int result;
+
+    result = a + b;
+    std::cout<< result <<std::endl;
+    return (0);
+}
