a566879f08d9dd3f07d5f104ccf3153fd020fcc8
Sabata <samofoke@student.wethinkcode.co.za>
2020-07-29 14:21:15 +0200

    sizeof operator

--- lesson1/casting.cpp
+++ lesson1/casting.cpp
@@ -6,10 +6,15 @@ int     main(void)
 {
     int x;
     float f;
+    char s;
+    s = 'a';
 
     f = 3.14;
     x = (int)f;
+    s = sizeof (char);
 
     std::cout << x << std::endl;
+    std::cout << s << std::endl;
+    printf("%d\n", s);
     return (0);
 }
