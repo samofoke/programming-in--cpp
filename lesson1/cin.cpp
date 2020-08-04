8e9eef3c0ad624302dd852a675d3e26cdde9698c
samofoke <samofoke@student.wethinkcode.co.za>
2020-08-04 16:23:28 +0200

    cin

---
+++ lesson1/cin.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int	main(void)
+{
+	int x;
+
+	std::cout << "please enter a number: ";
+	std::cin >> x;
+	std::cout << "the number has changed " << x * 2 << std::endl;
+	return (0);	
+}
