a3505e48992c34e945b643c5679f774fe0dc51ba
samofoke <samofoke@student.wethinkcode.co.za>
2020-08-10 17:59:46 +0200

    a for loop basic

---
+++ lesson1/forloop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// A for loop has a intialization, condition, statement and incrementing
+// the intialized value.
+
+int     main(void)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << "I am a number: " << i << std::endl;
+    }
+    return (0);
+}
