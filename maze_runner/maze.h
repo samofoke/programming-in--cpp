df84c36fde249b2598a31b578e4f122a03a73f6e
samofoke <samofoke@student.wethinkcode.co.za>
2020-10-11 18:33:16 +0200

    foundations

---
+++ maze_runner/maze.h
@@ -0,0 +1,11 @@
+/*******************************
+ *this is maze game with C++.
+ *inspired by kingsley.
+ *using the BFS algorithm.
+ * ****************************/
+
+#ifndef MAZE_H
+# define MAZE_H
+
+#endif
+
