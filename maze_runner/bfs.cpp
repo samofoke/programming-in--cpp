8d7ca6060be2cc9832ceefcc44fdd9b79e794cf6
samofoke <samofoke@student.wethinkcode.co.za>
2020-10-14 21:44:30 +0200

    building the bfs algo

--- maze_runner/bfs.cpp
+++ maze_runner/bfs.cpp
@@ -22,8 +22,45 @@ void    BFS(int x, int y)
                 Q.push(make_pair(xspx, xspy));
                 Quptd = 1;
 
-                
+                Qpre[xspx][xspy][0] = x;
+                Qpre[xspx][xspy][1] = y;
+                QnexNum[x][y]++;
+
+                if (srch_prcs)
+                {
+                    system("clear");
+                    PrintMase(xspx, xspy);
+                    if (!aut_ps)
+                    {
+                        printf("Please to continue.");
+                        getchar();
+                    }
+                    else
+                    {
+                        system(instructions);
+                    }
+                    if (xspx == tmlx && xspy ==tmly)
+                    {
+                        do
+                        {
+                            maze[xspx][xspy] = 15;
+                            int tmpx = Qpre[xspx][xspy][0];
+                            xspy = Qpre[xspx][xspy][1];
+                            xspx = tmpx;
+                        } while (xspx && xspy);
+                        
+                        system("clear");
+                        PrintMase(tmlx, tmly);
+                        Path_exit = 1;
+                        printf("We haave reached the end position.\n");
+                        printf("Press Enter to continue.");
+                        getchar();
+                        getchar();
+                    }
+                    
+                }
             }
         }
+        if () 
     }
 }
